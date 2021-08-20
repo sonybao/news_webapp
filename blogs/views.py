7384f4378072632c937d9912fb1461dfeb3d9abb
nguyenleha <65054260+nguyenleha@users.noreply.github.com>
2021-08-20 18:31:09 +0700

    asd

--- blogs/views.py
+++ blogs/views.py
@@ -10,7 +10,7 @@ from blogs.models import Post, Comment
 
 # Create your views here.
 def home_view(request):
-    object_list = Post.objects.all()
+    object_list = Post.objects.all().order_by('-date')
     return render(request, 'home.html', {
         'object_list': object_list
     })
@@ -18,12 +18,18 @@ def home_view(request):
 
 def about_view(request):
     return render(request, 'about.html')
+    
 
 
 class DetailPost(DetailView):
     model = Post
     template_name = 'detail_post.html'
-
+    
+    def listbaipost(request):
+        object_list_about = Post.objects.all().order_by('-date')
+        return render(request, 'detail_post.html', {
+            'object_list': object_list_about
+        })
 
 # def add_comment(request, pk):
 #     if request.method == 'POST':
